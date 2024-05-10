36f3ca1ea8dec183f2fe6df7790abbe6ab0fa10d
Giovanni Kane <it@linezerostudio.com>
2024-05-10 11:28:52 -0400

    Update page.tsx

--- app/freestyle/[difficulty]/page.tsx
+++ app/freestyle/[difficulty]/page.tsx
@@ -1,4 +1,4 @@
-import { generate, count } from 'random-words'
+// import { generate, count } from 'random-words'
 import { getWordByDifficulty, getWordData } from '../../server';
 import postgres from 'postgres';
 import FreestyleForm from '@/components/FreestyleForm';
@@ -7,8 +7,8 @@ import { Difficulty } from '@/types/difficulty';
 
 export default async function FreestylePage({params: {difficulty}}: {params: {difficulty: Difficulty}}) {
   // Function returns one by default but not as array so we explicitly define 1 for consistency
-  const words = generate(1);
-  const wordData = await getWordData(words.at(0) ?? "");
+  // const words = generate(1);
+  // const wordData = await getWordData(words.at(0) ?? "");
 
   const word = await getWordByDifficulty(difficulty);
 
