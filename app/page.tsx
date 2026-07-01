025985258047b80e389055af5f78ea094d24cfd7
Giovanni Kane <it@linezerostudio.com>
2026-06-30 23:37:06 -0400

    Remove home prerender database call

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
-import { generate, count } from 'random-words'
-import { getWordByDifficulty, getWordData } from './server';
-import postgres from 'postgres';
-import FreestyleForm from '@/components/FreestyleForm';
 import StartSection from '@/components/StartSection';
 
 export default async function Home() {
-  // Function returns one by default but not as array so we explicitly define 1 for consistency
-  const words = generate(1);
-  const wordData = await getWordData(words.at(0) ?? "");
-
-  const word = await getWordByDifficulty("medium");
-
   return (
     <main className='w-full max-w-[2560px] lg:mx-auto h-full'>
       <StartSection />
